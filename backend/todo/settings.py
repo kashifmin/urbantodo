b16258178c160e62d0bdcb633f550bf5cbafb4c8
Kashif Minhaj <dailypctips@gmail.com>
2018-04-09 11:09:32 +0530

    backend: Add CORS using django-cors-header

--- backend/todo/settings.py
+++ backend/todo/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'todoapi',
-    'tastypie'
-
+    'tastypie',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'todo.urls'
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# django-cors-headers
+CORS_ORIGIN_ALLOW_ALL = True # allow all origins for now
